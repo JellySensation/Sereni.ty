7545260d2d047aa2ed636c53ac317839f514926c
freegamerskids <60486718+freegamerskids@users.noreply.github.com>
2021-03-14 17:55:15 +0100

    update

---
+++ games/toh.lua
@@ -0,0 +1,54 @@
+local m = {}
+
+local win
+
+local tower = workspace.tower
+local sections = tower.sections
+
+local teleports = {
+    finish = sections.finish['FinishGlow'].CFrame,
+    floor = sections.start.floor['floor'].CFrame
+}
+
+local http = game:GetService("HttpService")
+local tp = game:GetService("TeleportService")
+
+function m:Init(winn)
+    win = winn
+    run()
+end
+
+function ServerHop()
+    function RandomServer()
+        local ts = {}
+        local stt = http:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Asc&limit=100"))
+        for i,v in pairs(stt.data) do
+            if v.playing ~= v.maxPlayers then
+                table.insert(ts,v)
+            end
+        end
+        return ts[math.random(1,#ts)]
+    end
+    tp:TeleportToPlaceInstance(game.PlaceId, RandomServer().id)
+end
+
+function run()
+
+    local im = win:Tab('AutoFarm')
+
+    local autofarm = im:Button('AutoFarm',function()
+        for i,v in pairs(sections:GetChildren()) do
+            if v.Name =='finish' or 'start' then return end
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.start
+            wait(2)
+        end
+
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = teleports.finish
+    
+        syn.queue_on_teleport(loadstring(game:HttpGetAsync('https://pastebin.com/raw/2JWbxTQp',true))())
+        ServerHop()
+    end)
+end
+
+return m
--- main.lua
+++ main.lua
@@ -1,1 +1,17 @@
-local lib = loadstring(game:HttpGet())
+repeat wait() until game:IsLoaded()
+
+local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/Library.lua',true))()
+
+_G.VERSION = 'v0.0.1'
+
+local games = {
+    [703124385] = 'toh' -- Tower Of Hell
+}
+
+local pg = games[game.GameId]
+
+local win = lib:Window('Sereni.ty', Color3.new(1, 0, 0))
+
+if pg == nil then return game.Players.LocalPlayer:Kick('not coolio') else
+    require(loadstring(game:HttpGet('https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/'..pg..'.lua', true))):Init(win)
+end
