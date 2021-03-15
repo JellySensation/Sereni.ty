cc3b9bbc1471e92122a507e589c93e5ed4c2d091
freegamerskids <60486718+freegamerskids@users.noreply.github.com>
2021-03-15 18:41:56 +0100

    autoupdate

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
-repeat wait() until game:IsLoaded()
+repeat wait() until game['IsLoaded']()
 
-local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/Library.lua',true))()
+local lib = loadstring(game['HttpGet']('https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/Library.lua',true))()
 
 _G.VERSION = 'v0.0.1'
 
@@ -8,10 +8,10 @@ local games = {
     [703124385] = 'toh' -- Tower Of Hell
 }
 
-local pg = games[game.GameId]
+local pg = games[game['GameId']]
 
-local win = lib:Window('Sereni.ty', Color3.new(1, 0, 0))
+local win = lib['Window']('Sereni.ty', Color3.new(1, 0, 0))
 
 if pg == nil then return game.Players.LocalPlayer:Kick('not coolio') else
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/'..pg..'.lua', true))(win)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/'..pg..'.lua', true))()(win)
 end
