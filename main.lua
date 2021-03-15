2f6c73a2ba6ceed3c4916ee58fd85a8514cd1aa7
freegamerskids <60486718+freegamerskids@users.noreply.github.com>
2021-03-15 18:42:50 +0100

    autoupdate

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
-repeat wait() until game['IsLoaded']()
+repeat wait() until game:IsLoaded()
 
-local lib = loadstring(game['HttpGet']('https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/Library.lua',true))()
+local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/Library.lua',true))()
 
 _G.VERSION = 'v0.0.1'
 
@@ -10,7 +10,7 @@ local games = {
 
 local pg = games[game['GameId']]
 
-local win = lib['Window']('Sereni.ty', Color3.new(1, 0, 0))
+local win = lib:Window('Sereni.ty', Color3.new(1, 0, 0))
 
 if pg == nil then return game.Players.LocalPlayer:Kick('not coolio') else
     loadstring(game:HttpGet('https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/'..pg..'.lua', true))()(win)
