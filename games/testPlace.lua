c9e9b583eada98d52bf9fecc03ec97a28cc2b042
Samuel Misenka <freegamerskids@gmail.com>
2021-10-26 08:25:18 +0200

    aaaaaa

---
+++ games/testPlace.lua
@@ -0,0 +1,4 @@
+return function(win)
+    local abctest = win:Tab('domah')
+    local label = abctest:Label('balls lololololol')
+end
--- main.lua
+++ main.lua
@@ -6,7 +6,8 @@ _G.VERSION = 'v0.0.1'
 
 local games = {
     [703124385] = 'https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/toh.lua', -- Tower Of Hell
-    [6284583030] = 'https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/petsimx.lua', -- Pet Simulator X
+    [2316994223] = 'https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/petsimx.lua', -- Pet Simulator X
+    [2533050278] = 'https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/testPlace.lua' -- im testing my shit
 }
 
 local pg = games[game['GameId']]
