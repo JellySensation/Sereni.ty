07d1f98cf421095b817b68641ae2b9bb36934d85
freegamerskids <60486718+freegamerskids@users.noreply.github.com>
2021-12-19 18:56:15 +0100

    universal shit

--- main.lua
+++ main.lua
@@ -4,16 +4,22 @@ local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/freegamer
 
 _G.VERSION = 'v0.0.1'
 
+function r(sName)
+    return 'https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/'..sName..'.lua'
+end
+
 local games = {
-    [703124385] = 'https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/toh.lua', -- Tower Of Hell
-    [2316994223] = 'https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/petsimx.lua', -- Pet Simulator X
-    [2533050278] = 'https://raw.githubusercontent.com/freegamerskids/Sereni.ty/main/games/testPlace.lua' -- im testing my shit
+    [703124385] = r('toh'), -- Tower Of Hell
+    [2316994223] = r('petsimx'), -- Pet Simulator X
+    [2533050278] = r('testPlace') -- im testing my shit
 }
 
 local pg = games[game['GameId']]
 
 local win = lib:Window('Sereni.ty', Color3.new(1, 0, 0))
 
-if pg == nil then return game.Players.LocalPlayer:Kick('not coolio') else
+if pg == nil then 
+    loadstring(game:HttpGetAsync(r('universal'), true))():Init(win)
+else
     loadstring(game:HttpGetAsync(pg, true))():Init(win)
 end
