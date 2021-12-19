d73a46c99524ad33a64f1fce8d20e99a074c6e04
freegamerskids <60486718+freegamerskids@users.noreply.github.com>
2021-12-19 20:53:43 +0100

    Update testPlace.lua

--- games/testPlace.lua
+++ games/testPlace.lua
@@ -3,7 +3,7 @@ local m = {}
 function m:Init(win)
     local abctest = win:Tab('domah')
     local label = abctest:Label('balls lololololol')
-    local textBox = abctest:TextBox('yo',false,function(text)
+    local textBox = abctest:Textbox('yo',false,function(text)
         label:Edit(text)
     end)
 end
